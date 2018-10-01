f13cd8bd25b76fe885d00f5166da05103e86e27a
LeoYoung <yangtao@23mofang.com>
2018-10-01 23:48:02 +0800

    feat: add view container

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import yaml from 'js-yaml';
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
@@ -14,17 +15,30 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    'extension.sayHello',
-    () => {
-      // The code you place here will be executed every time your command is executed
-
-      // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World!');
+  const openTextBeside = async () => {
+    if (vscode.window.activeTextEditor) {
+      const text = vscode.window.activeTextEditor.document.getText();
+      const content = JSON.stringify(yaml.load(text));
+      vscode.window.showTextDocument(
+        await vscode.workspace.openTextDocument({
+          content,
+          language: 'plain'
+        }),
+        vscode.ViewColumn.Beside,
+        true
+      );
     }
+  };
+  const disposable = vscode.commands.registerCommand(
+    'l-yaml.viewTree',
+    openTextBeside
   );
 
   context.subscriptions.push(disposable);
+
+  // disposable = vscode.window.onDidChangeTextEditorSelection(openTextBeside);
+
+  // context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
